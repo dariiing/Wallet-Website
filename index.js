053137566008431010173b7b8d2e20b57a34699d
daria <dariae9@yahoo.com>
2023-03-19 17:26:21 +0200

    popup

--- index.js
+++ index.js
@@ -2,3 +2,45 @@ function themeToggle() {
   var element = document.body;
   element.classList.toggle("dark-theme");
 }
+
+Orders.forEach(order => {
+  const tr = document.createElement('tr');
+  const trContent = `
+                    <td>${order.name}</td>
+                    <td>${order.date}</td>
+                    <td>${order.price}</td>
+                    <td>${order.type}</td>
+                    `;
+  tr.innerHTML = trContent;
+  document.querySelector('table tbody').appendChild(tr);
+})
+
+function logExpense(){
+  Orders.push({
+    name: 'Gas Bill',
+    date: '04/03/2023',
+    price: 150,
+    type: 'expense'
+  });
+
+  const lastOrder = Orders[Orders.length - 1];
+  const tr = document.createElement('tr');
+  const trContent = `
+                    <td>${lastOrder.name}</td>
+                    <td>${lastOrder.date}</td>
+                    <td>${lastOrder.price}</td>
+                    <td>${lastOrder.type}</td>
+                    `;
+  tr.innerHTML = trContent;
+  document.querySelector('table tbody').appendChild(tr);
+}
+
+function openPopup(){
+  let popup = document.getElementById("popup");
+  popup.classList.add("open-popup");
+}
+
+function closePopup(){
+  let popup = document.getElementById("popup");
+  popup.classList.remove("open-popup");
+}
---
+++ orders.js
@@ -0,0 +1,26 @@
+const Orders = [
+    {
+        name: 'Electricity Bill',
+        date: '03/03/2023',
+        price: 234,
+        type: 'expense'
+    },
+    {
+        name: 'Pepco',
+        date: '02/03/2023',
+        price: 120,
+        type: 'expense'
+    },
+    {
+        name: 'Brunch',
+        date: '01/03/2023',
+        price: 250,
+        type: 'expense'
+    },
+    {
+        name: 'Scolarship',
+        date: '03/03/2023',
+        price: 500,
+        type: 'income'
+    }
+]

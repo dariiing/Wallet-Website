493fd270063c47d859b5bf4dc54b8e1c66ef9b05
daria <dariae9@yahoo.com>
2023-04-08 12:28:06 +0300

    side buttons work

--- index.js
+++ index.js
@@ -44,3 +44,31 @@ function closePopup(){
   let popup = document.getElementById("popup");
   popup.classList.remove("open-popup");
 }
+
+function openWallet(){
+  let popup = document.getElementById("wallet-form");
+  popup.classList.add("open-wallet-form");
+  popup = document.getElementById("sidebar");
+  popup.classList.add("sidebar-height");
+}
+
+function closeWallet(){
+  let popup = document.getElementById("wallet-form");
+  popup.classList.remove("open-wallet-form");
+  popup = document.getElementById("sidebar");
+  popup.classList.remove("sidebar-height");
+}
+
+function openCategory(){
+  let popup = document.getElementById("category-form");
+  popup.classList.add("open-category-form");
+  popup = document.getElementById("sidebar");
+  popup.classList.add("sidebar-height");
+}
+
+function closeCategory(){
+  let popup = document.getElementById("category-form");
+  popup.classList.remove("open-category-form");
+  popup = document.getElementById("sidebar");
+  popup.classList.remove("sidebar-height");
+}
